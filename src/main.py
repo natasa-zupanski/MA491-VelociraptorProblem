e05569b2586e2a22e65270148810206c94fcfdd6
natasa-zupanski <zupansnm@rose-hulman.edu>
2024-11-11 19:42:11 -0500

    new stuff

--- src/main.py
+++ src/main.py
@@ -1,38 +1,47 @@
 import tensorflow as tf
+from tensorflow.python.keras.layers import Dense, Input
+from tensorflow.python.keras import Sequential
 
 
-# class Constants :
-#     time-step = 0.01
-#     inp-size = (8,1,1)
+class Constants:
+    time_step = 0.01
+    inp_size = (8,1,1)
+    max_time = 15
+    velo_v_max = 60
+    thes_v_max = 50
 
 
 class Main:
     
-    def runRound(self, predator, prey) :
+    def runRound(self, predator, prey, trials) :
         lil = None
-
-    def runMain(self, loadFile, saveFile) :
-        model = None
-        # constants = Constants()
         
-        # if (loadFile == None or len(loadFile) == 0) :
-        #     # create new model
-        #     predator = Sequential([
-        #         Dense(units=84, activation="relu"),
-        #         Dense(units=10, activation="softmax"),
-        #     ])
-            
-        #     prey = Sequential([
-        #         Dense(units=84, activation="relu"),
-        #         Dense(units=10, activation="softmax"),
-        #     ])
 
-        # else :
-        #     # load old model
-        #     model = None
-        # print("WH")  
-        #runRound(predator, prey)
+    def runMain(self, loadFile, saveFile, rounds) :
+
+        constants = Constants()
+       # print(tf._kernel_dir.)
+        
+        if (loadFile == None or len(loadFile) == 0) :
+            # create new model
+            predator = Sequential([
+                Input(shape=constants.inp_size),
+                Dense(units=84, activation="relu"),
+                Dense(units=10, activation="softmax"),
+            ])
+            
+            prey = Sequential([
+                Input(shape=constants.inp_size),
+                Dense(units=84, activation="relu"),
+                Dense(units=10, activation="softmax"),
+            ])
+
+        else :
+            # load old model
+            model = None
+            
+        self.runRound(predator, prey, rounds)
         
         
 main = Main()
-main.runMain(1,2)
+main.runMain(1,2,1)
